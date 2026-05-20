749d53f54d90b63cefff566e60918b53c4844d2b
valentintomassanchezdibella-creator <valentintomassanchezdibella@gmail.com>
2026-05-20 15:22:44 -0300

    subida a netlify

--- frontend/admin/script.js
+++ frontend/admin/script.js
@@ -1,4 +1,4 @@
-const API = 'http://127.0.0.1:8000';
+const API = 'https://preguntas-frecuentes-castores.onrender.com';
 let token = localStorage.getItem('token') || '';
 let currentUser = null;
 let preguntasData = [];
